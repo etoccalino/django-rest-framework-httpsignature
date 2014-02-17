922598b5805fc37d9972ed962fdd4e804253977e
Elvio Toccalino <etoccalino@creativa77.com.ar>
2014-02-17 19:29:55 -0300

    improved setup script

--- setup.py
+++ setup.py
@@ -5,10 +5,21 @@ setup(
     name='djangorestframework-httpsignature',
     version='0.1.0',
     url='https://github.com/etoccalino/django-rest-framework-httpsignature',
+
     license='LICENSE.txt',
     description='HTTP Signature support for Django REST framework',
     long_description=open('README.txt').read(),
-    install_requires=open('REQUIREMENTS.txt').read().split('\n'),
+
+    install_requires=[
+        'Django==1.6.2',
+        'djangorestframework==2.3.12',
+        'pycrypto==2.6.1',
+        'http_signature'
+    ],
+    dependency_links=[
+        'https://github.com/etoccalino/py-http-signature/archive/v0.2.0.zip#egg=http_signature-0.2.0',
+    ],
+
     author='Elvio Toccalino',
     author_email='me@etoccalino.com',
     packages=['rest_framework_httpsignature'],
