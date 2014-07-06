6cbd61dc88b741ade422b3e65edadb10f3e9fbd9
Elvio Toccalino <me@etoccalino.com>
2014-07-06 10:46:41 -0300

    bump version for API change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='djangorestframework-httpsignature',
-    version='0.1.5',
+    version='0.1.6',
     url='https://github.com/etoccalino/django-rest-framework-httpsignature',
 
     license='LICENSE.txt',
