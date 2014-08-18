4528c6456aee92f5d0b622a74971172562d76d71
Elvio Toccalino <me@etoccalino.com>
2014-08-18 13:48:18 -0300

    bump to version 0.1.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='djangorestframework-httpsignature',
-    version='0.1.7',
+    version='0.1.8',
     url='https://github.com/etoccalino/django-rest-framework-httpsignature',
 
     license='LICENSE.txt',
