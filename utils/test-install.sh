ac5194bd1763f95e4876fc4d26dc4705dfeb2293
Elvio Toccalino <me@etoccalino.com>
2014-08-18 12:51:56 -0300

    added a script to test installation

---
+++ utils/test-install.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+#
+# A simple & quick script to test that a local development source distribution
+# can be successfully installed in a fresh virtual environment via pip.
+#
+
+
+REPO_DIR=""        # Where the source code lives (to build the dist)
+VIRTUALENV_DIR=""  # Where the source code virtualenv lives (to build the dist)
+DEST_DIR=""        # Where to install. Must not exist.
+
+# Example:
+#   REPO_DIR="$HOME/programming/django-rest-framework-httpsignature"
+#   VIRTUALENV_DIR="${REPO_DIR}/env"
+#   DEST_DIR="$HOME/tmp/t"
+
+set -e
+set -x
+
+# Build the source distribution
+cd ${REPO_DIR}
+source ${VIRTUALENV_DIR}/bin/activate
+rm -rf dist/
+python setup.py build sdist
+
+# Move to the distination directory, create the test environment and instal.
+mkdir ${DEST_DIR}
+cd ${DEST_DIR}
+virtualenv .
+source bin/activate
+pip install ${REPO_DIR}/dist/*
+
+# The installation was successful. Destroy the test environment.
+cd ${REPO_DIR}
+rm -fr ${DEST_DIR}
